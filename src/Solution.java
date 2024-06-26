457ea9b19141ddd9e42ad6bb890b6ade513b8c02
LakshithaMaduranga <155385829+LakshithaMaduranga@users.noreply.github.com>
2024-06-26 09:05:06 +0530

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc= new Scanner(System.in);
+        //System.out.println("Enter first integer value here: ");
+        int input1= sc.nextInt();
+
+        //System.out.println("Enter Second integer value here: ");
+        int input2= sc.nextInt();
+
+        //System.out.println("Enter thard integer value here: ");
+        int input3= sc.nextInt();
+
+        System.out.println(input1);
+
+        System.out.println(input2);
+
+        System.out.println(input3);
+
+
+
+
+    }
+}
